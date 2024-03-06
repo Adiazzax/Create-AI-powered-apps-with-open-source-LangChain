fd3864dd351ebc0b9edef03bd1c561d438f74dcb
Adi Karawinata <adiazzax@gmail.com>
2024-03-06 15:31:51 +0700

    Update coderunner.py

--- coderunner.py
+++ coderunner.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
 from langchain_experimental.utilities import PythonREPL
 
-openai_api_key = "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"
+openai_api_key = "OpenaiAI API"
 
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
